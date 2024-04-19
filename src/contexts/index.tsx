91051a0313809535cf4279462ca300ed6750d036
tidvn <tiendung0325@gmail.com>
2024-04-19 19:32:24 +0700

    draft

---
+++ src/contexts/index.tsx
@@ -0,0 +1,25 @@
+"use client"
+import React, { ReactNode, lazy } from "react";
+
+const LucidProvider = lazy(() => import("./LucidProvider"));
+const SmartcontractProvider = lazy(() => import("./SmartcontractProvider"));
+const WalletProvider = lazy(() => import("./WalletProvider"));
+const AccountProvider = lazy(() => import("./AccountProvider"));
+
+type Props = {
+    children: ReactNode;
+};
+
+const ContextProvider = function ({ children }: Props) {
+    return (
+        <LucidProvider>
+            <SmartcontractProvider>
+                <WalletProvider>
+                    <AccountProvider>{children}</AccountProvider>
+                </WalletProvider>
+            </SmartcontractProvider>
+        </LucidProvider>
+    );
+};
+
+export default ContextProvider;
